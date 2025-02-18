b89fdda27c11527793fc040fb872ed32760367bc
Angela Capella <angela.capella950@comunidadunir.net>
2025-02-18 20:08:21 +0000

    haciedo prueba de commit

--- src/main/java/Calculadora/Resta.java
+++ src/main/java/Calculadora/Resta.java
@@ -2,40 +2,50 @@ package Calculadora;
 
 public class Resta {
 
+    private double acumulado;
+    public Resta(){
+        acumulado=0.0;
+    }
+
     // Primer metodo: Resta de dos números reales,
     // tendrá 2 parámetros de entrada y uno de salida que será la solución.
 
-    public double restaReales(double numb1,double numb2) {
-        return numb1  - numb2;
+    public double restaReales(double a,double b) {
+        return a  - b;
     }
+
     // Segundo metodo:Resta de dos números enteros,
     // tendrá 2 parámetros de entrada y uno de salida que será la solución.
 
-    public int restaEnteros(int numb1,int numb2){
-        return numb1 - numb2;
+    public int restaEnteros(int a,int b){
+        return a - b;
     }
 
     //Tercer metodo:Resta de tres números reales, tendrá 3 parámetros de entrada
     // y uno de salida que será la solución.
 
-    public double restarReales(double numb1,double numb2,double numb3){
-        return numb1 - numb2 - numb3;
+    public double restarReales(double a,double b,double c){
+        return a - b - c;
     }
 
     //Cuarto metodo: Resta con valor acumulado, tendrá un parámetro de entrada
     // y la clase deberá de guardar el valor acumulado.
 
-    public double restaAcumulada(double numb){
-        valorAcumulado -= numb;
-        return valorAcumulado;
+    public double restaAcumulado(double valor){
+        acumulado -= valor;
+        return acumulado;
     }
 
-    public double getValorAcumulado(){
-        return valorAcumulado;
+    public double getAcumulado(){
+        return acumulado;
+    }
+
+
     }
 
 
 
-}
+
+
 
 
