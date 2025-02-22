16996f45a04e0f2e308c4b2abdbc6993fa502f22
Jose Maria Castellano <castellanousero@gmail.com>
2025-02-22 05:03:26 +0100

    Creacion Clase Potencia

---
+++ src/main/java/Calculadora/Potencia.java
@@ -0,0 +1,62 @@
+package Calculadora;
+
+/**
+ * Clase que realiza operaciones matemáticas.
+ * Esta clase implementará todos los métodos que están relacionados con la multiplicación de la calculadora.
+ * @author josemariacastellano
+ * @version 1.0
+
+ */
+
+public class Potencia {
+
+    /**
+     * Producto de dos números reales y devuelve solucion.
+
+     * @param Reala Primer numero real a multiplcar.
+     * @param Realb Segundo numero real a multiplicar.
+     * @return Resultado de la multiplicación de Reala y Realb.
+     */
+    public double potenciareales(double Reala, double Realb) {
+        return Reala * Realb;
+    }
+
+
+    /**
+     * Producto de dos números enteros y devuelve solucion.
+     *
+     * @param Enteroa Primer numero entero a multiplcar.
+     * @param Enterob Segundo numero entero a multiplicar.
+     * @return Resultado de la multiplicación de Enteroa y Enterob.
+     */
+    public int potenciaenteros(int Enteroa, int Enterob) {
+        return Enteroa * Enterob;
+    }
+
+
+    /**
+     * Producto de tres números reales y devuelve solucion.
+
+     * @param Reala Primer numero real a multiplcar.
+     * @param Realb Segundo numero real a multiplicar.
+     * @param Realc Tercer numero real a multiplicar.
+     * @return Resultado de la multiplicación de Reala, Realb y Realc.
+     */
+    public double potenciatresreales(double Reala, double Realb, double Realc) {
+        return Reala * Realb * Realc;
+    }
+
+    /**
+     * Producto de tres números reales y devuelve solucion.
+
+     * @param Base Base al que se elevará.
+     * @param Exponente Exponente que se elevará la base.
+     * @return Resultado del Exponente elevado a la Base
+    */
+    public double potencia(double Base, int Exponente) {
+        return Math.pow(Base, Exponente);
+    }
+
+
+
+}
