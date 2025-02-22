f0721b706af4b239903c4ce08194bddcc9fb1e11
Angela Capella <angela.capella950@comunidadunir.net>
2025-02-22 12:03:01 +0000

    ADD:Testeando metodos resta

--- src/main/java/Calculadora/Main.java
+++ src/main/java/Calculadora/Main.java
@@ -22,6 +22,12 @@ public class Main {
          */
 
         //Pruebas de Resta
-        System.out.println("La resta de dos numeros reales : " + resta. );
+        Resta resta= new Resta();
+        System.out.println("La resta de dos numeros reales : " + resta.restaReales(8.5, 6.5));
+        System.out.println("La resta de dos numeros enteros : " + resta.restaEnteros(10, 7));
+        System.out.println("La resta de tres números reales : " + resta.restarReales(8.3,2.2,1.3));
+        System.out.println("La resta con un valor acumulado : " + resta.restaAcumulado(7));
+
+
     }
 }
