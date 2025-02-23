9b0d5e6f9665e810649ce4863b9862d34d948007
Angela Capella <angela.capella950@comunidadunir.net>
2025-02-23 13:16:22 +0000

    UPDATE: modificando en mi rama

--- src/main/java/Calculadora/Main.java
+++ src/main/java/Calculadora/Main.java
@@ -44,13 +44,9 @@ public class Main {
         System.out.println("Potencia de base por exponente" + producto.productopotencia(8, 4));
 
 
-        //Pruebas de Resta
 
-        Resta resta= new Resta();
-        System.out.println("La resta de dos numeros reales : " + resta.restaReales(8.5, 6.5));
-        System.out.println("La resta de dos numeros enteros : " + resta.restaEnteros(10, 7));
-        System.out.println("La resta de tres números reales : " + resta.restarReales(8.3,2.2,1.3));
-        System.out.println("La resta con un valor acumulado : " + resta.restaAcumulado(7));
+
+
 
  
     }
