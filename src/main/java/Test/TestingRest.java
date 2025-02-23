dd43981be43e1cc9127793edaf46ae9dd44747ad
Angela Capella <angela.capella950@comunidadunir.net>
2025-02-23 13:23:31 +0000

    ADD: agregando test de resta

--- src/main/java/Test/TestingRest.java
+++ src/main/java/Test/TestingRest.java
@@ -1,4 +1,17 @@
 package Test;
 
+import Calculadora.Resta;
+
 public class TestingRest {
+    public static void main(String[] args) {
+        /**
+         * Pruebas de resta
+         */
+
+        Resta resta= new Resta();
+        System.out.println("La resta de dos numeros reales : " + resta.restaReales(8.5, 6.5));
+        System.out.println("La resta de dos numeros enteros : " + resta.restaEnteros(10, 7));
+        System.out.println("La resta de tres números reales : " + resta.restarReales(8.3,2.2,1.3));
+        System.out.println("La resta con un valor acumulado : " + resta.restaAcumulado(7));
+    }
 }
