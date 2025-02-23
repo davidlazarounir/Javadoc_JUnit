241335c04cee2e22fe09a63d0f744a5b14f64e29
Jose Maria Castellano <castellanousero@gmail.com>
2025-02-24 00:59:53 +0100

    Implementación del mensaje de error si intentamos introducir un "0" como valor

--- src/test/java/Calculadora/ProductoTest.java
+++ src/test/java/Calculadora/ProductoTest.java
@@ -1,38 +1,50 @@
 package Calculadora;
 
-import Calculadora.Producto;
+
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ProductoTest {
 
     private Producto producto;
 
     @BeforeEach
-    void setUp() { producto = new Producto();
+    void setUp() {
+        producto = new Producto();
     }
 
     @Test
+    @DisplayName("Prodcuto de dos numeros reales")
     void prodcutoreales() {
-        assertEquals(4.0, producto.prodcutoreales(2, 2), 0.0001);
+        assertEquals(4.0, producto.prodcutoreales(2.0, 2.0), 0.0001);
     }
 
     @Test
+    @DisplayName("Producto de dos numeros enteros")
     void productoenteros() {
         assertEquals(-10, producto.productoenteros(-5, 2));
     }
 
-    @Test
-    void productotresreales() {
-        assertEquals(0.0, producto.productotresreales(0, 2.43, 44.69), 0.0001);
-    }
+
 
     @Test
-    void productopotencia() {
-        assertEquals(1.0, producto.productopotencia(5, 0), 0.0001);
+    @DisplayName("Test tres numeros reales")
+    void productotresreales() {
+        assertEquals(108.5967, producto.productotresreales(1.0, 2.43, 44.69), 0.0001);
     }
+
+
+
+    @Test
+    @DisplayName("Test Potencia")
+    void productopotencia() {
+        assertEquals(16.0, producto.productopotencia(2.0, 4), 0.0001);
+    }
+
 
 
 }
