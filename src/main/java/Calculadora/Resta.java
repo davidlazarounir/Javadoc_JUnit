20eba576e4724a52831424356bad72d2382720ab
Angela Capella <angela.capella950@comunidadunir.net>
2025-02-23 13:20:42 +0000

    Comentarios javaDoc en mi rama

--- src/main/java/Calculadora/Resta.java
+++ src/main/java/Calculadora/Resta.java
@@ -7,29 +7,29 @@ public class Resta {
         acumulado=0.0;
     }
 
-    // Primer metodo: Resta de dos números reales,
-    // tendrá 2 parámetros de entrada y uno de salida que será la solución.
+    /**Primer metodo: Resta de dos números reales,
+    tendrá 2 parámetros de entrada y uno de salida que será la solución.*/
 
     public double restaReales(double a,double b) {
         return a  - b;
     }
 
-    // Segundo metodo:Resta de dos números enteros,
-    // tendrá 2 parámetros de entrada y uno de salida que será la solución.
+    /** Segundo metodo:Resta de dos números enteros,
+    tendrá 2 parámetros de entrada y uno de salida que será la solución.**/
 
     public int restaEnteros(int a,int b){
         return a - b;
     }
 
-    //Tercer metodo:Resta de tres números reales, tendrá 3 parámetros de entrada
-    // y uno de salida que será la solución.
+    /**Tercer metodo:Resta de tres números reales, tendrá 3 parámetros de entrada
+     y uno de salida que será la solución.*/
 
     public double restarReales(double a,double b,double c){
         return a - b - c;
     }
 
-    //Cuarto metodo: Resta con valor acumulado, tendrá un parámetro de entrada
-    // y la clase deberá de guardar el valor acumulado.
+    /**Cuarto metodo: Resta con valor acumulado, tendrá un parámetro de entrada
+     y la clase deberá de guardar el valor acumulado.*/
 
     public double restaAcumulado(double valor){
         acumulado -= valor;
