7d448960101e6366310abaf08f704b04f384a955
Davidlazarounir <david.lazaro618@comunidadunir.net>
2025-02-24 20:19:41 +0100

    UPDATE: comentarios en Suma

--- src/main/java/Calculadora/Suma.java
+++ src/main/java/Calculadora/Suma.java
@@ -1,9 +1,15 @@
 package Calculadora;
 
 /**
- * Clase que proporciona operaciones matemáticas básicas
+ * Clase que proporciona operaciones matemáticas básicas relacionadas con la suma.
+ * Incluye la suma de:
+ * 1- dos numeros reales
+ * 2- dos numeros enteros
+ * 3- tres numeros reales
+ * 4 - un número acumulado
+ *
  * @author David Lázaro (davidlazarounir en GitHub)
- * @version 1.3
+ * @version 1.4
  */
 
 public class Suma {
@@ -41,7 +47,7 @@ public class Suma {
 
     /**
      * Suma valor acumulado, tendrá un parámetro de entrada y la clase deberá de guardar el valor acumulado
-     * @param valor, es el numero que se introduce y sustituye al acumulado, guardando la cifra
+     * @param valor, es el numero introducimos en la calculadora. Se sumará al acumulado.
      * @return
      */
 
@@ -50,10 +56,16 @@ public class Suma {
         return this.acumulado;
     }
 
+    /**
+     * Indica el número acumulado actual.
+     */
     public double getAcumulado() {
         return this.acumulado;
     }
 
+    /**
+     * Convierte el número acumuado a (0)
+     */
     public void reiniciarAcumulado() {
         this.acumulado = 0;
     }
