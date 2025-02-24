f9504b8cd8a2f68206ae3f7800cc9d3ecc462941
WorldOfPromise <correo.online.de.david@gmail.com>
2025-02-24 22:36:15 +0100

    MERGE: hago la clase ProductoTest.java  -- errores con RestaTest.java por directorio

--- src/test/java/Calculadora/ProductoTest.java
+++ src/test/java/Calculadora/ProductoTest.java
@@ -1,11 +1,43 @@
 package Calculadora;
 
+/**
+ * Clase que realiza operaciones matemáticas.
+ * Esta clase implementará todos los métodos que están relacionados con la multiplicación de la calculadora.
+ * @author WorldOfPromise y el resto de compañeros del Repositorio
+ * @version 1.0
+
+ */
+
+import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductoTest {
+
+    /**
+     * Clase de pruebas unitarias para verificar el correcto funcionamiento de los métodos
+     * de la clase {@link Producto}.
+     *
+     * Con pruebas para:
+     * Producto de dos números reales
+     * Producto de dos números enteros
+     * Producto de tres números reales
+     * Cálculo de potencias
+     * </ul>
+     *
+     * @author WorldOfPromise
+     * @version 1.0
+     * @see Producto
+     */
+
+    @Test
+    public void testprodcutoreales() {}
+    Producto producto = new Producto();
+    //Ejemplo 1: 5.0 * 2.0 = 10.0
+
 
     @BeforeEach
     void setUp() {
@@ -25,5 +57,45 @@ class ProductoTest {
 
     @Test
     void productopotencia() {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.Test
+    public void testProdcutoreales() {
+    }
+
+    @org.junit.Test
+    public void testProductoenteros() {
+    }
+
+    @org.junit.Test
+    public void testProductotresreales() {
+    }
+
+    @org.junit.Test
+    public void testProductopotencia() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testProdcutoreales1() {
+    }
+
+    @Test
+    void testProductoenteros1() {
+    }
+
+    @Test
+    void testProductotresreales1() {
+    }
+
+    @Test
+    void testProductopotencia1() {
     }
 }
---
+++ src/test/java/Test/ProductoTest.java
@@ -0,0 +1,29 @@
+package Test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void prodcutoreales() {
+    }
+
+    @Test
+    void productoenteros() {
+    }
+
+    @Test
+    void productotresreales() {
+    }
+
+    @Test
+    void productopotencia() {
+    }
+}
